7cdfb1228ccea734ad12da57e3298a14c3b2136b
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-09-25 20:06:23 +0700

    menambahkan kelas coba, mobil dan produk

---
+++ Mobil.php
@@ -0,0 +1,24 @@
+<?php 
+
+	class Mobil {
+		//property
+		public $nama;
+		public $merk;
+		public $warna;
+		public $kecepatanMaksimal;
+		public $jumlahPenumpang;
+
+		public function tambahKecepatan(){
+			return $this->kecepatan += 10;
+		}
+	}
+
+	$avanza = new Mobil;
+	$avanza->nama = 'Avanza';
+	$avanza->merk = 'Toyota';
+	$avanza->warna = 'silver';
+	$avanza->tambahKecepatan();
+
+	var_dump($avanza);
+
+ ?>
---
+++ Produk.php
@@ -0,0 +1,10 @@
+<?php 
+
+ class Produk{
+
+ }
+
+ $produk1 = new Produk();
+ $Produk2 = new Produk();
+
+ ?>
