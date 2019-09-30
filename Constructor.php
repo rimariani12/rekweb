0b8c381d17e9a00a13585985ec8c352361209375
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-09-30 09:56:45 +0700

    merubah contructor.php

--- Constructor.php
+++ Constructor.php
@@ -2,13 +2,13 @@
 
  class Produk{
  	//property
- 	public  $judul = "judul",
- 			$penulis = "penulis",
- 			$penerbit = "penerbit",
- 			$harga = 30000;
+ 	public  $judul,
+ 			$penulis,
+ 			$penerbit,
+ 			$harga;
 
  			//konstrktor
- 			public function __construct( $judul, $penulis, $penerbit, $harga){
+ 			public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){
  				$this->judul = $judul;
  				$this->penulis = $penulis;
  				$this->penerbit = $penerbit;
@@ -26,8 +26,11 @@
 //instansiasi //object
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk3 = new Produk("Dragon Ball");
 
 
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel(); 
+echo "<br>";
+var_dump($produk3);
